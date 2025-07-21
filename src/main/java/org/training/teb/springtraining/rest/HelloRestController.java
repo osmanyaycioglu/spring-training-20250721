024d2f9f7c9a4345a47265bfe3403f21832980dc
osman yaycioglu <osman.yaycioglu@gmail.com>
2025-07-21 16:09:38 +0300

    day 1

---
+++ src/main/java/org/training/teb/springtraining/rest/HelloRestController.java
@@ -0,0 +1,66 @@
+package org.training.teb.springtraining.rest;
+
+import org.springframework.web.bind.annotation.*;
+
+//@Repository
+//@Service
+//@Configuration
+
+//@Controller
+//@ResponseBody
+
+@RestController
+@RequestMapping("/my")
+public class HelloRestController {
+
+    // @RequestMapping(path = "/my/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost() {
+        return "hello - POST";
+    }
+
+    @PutMapping("/hello")
+    public String helloPut() {
+        return "hello - PUT";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam("name") String name) {
+        return "Greetings " + name;
+    }
+
+    @GetMapping("/greeting2")
+    public String greeting(@RequestParam("name") String name,
+                           @RequestParam("surname") String surname) {
+        return "Greetings " + name + " " + surname;
+    }
+
+    @GetMapping("/greeting3/{name}")
+    public String greeting3(@PathVariable("name") String name) {
+        return "Greetings 3 " + name;
+    }
+
+    @GetMapping("/greeting4/{name}/sss/{sn}")
+    public String greeting4(@PathVariable("name") String name,
+                            @PathVariable("sn") String surname) {
+        return "Greetings 4 " + name + " " + surname;
+    }
+
+    @GetMapping("/greeting5/{name}")
+    public String greeting5(@PathVariable("name") String name,
+                            @RequestParam("surname") String surname) {
+        return "Greetings 5 " + name + " " + surname;
+    }
+
+
+    @GetMapping("/goodbye")
+    public String goodbye() {
+        return "goodbye";
+    }
+
+}
